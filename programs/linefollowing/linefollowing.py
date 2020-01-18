8661af8de29e5f9ecb1fe2f433cd02b5cca1139d
estods3 <estods76@gmail.com>
2020-01-18 14:15:23 -0800

    having trouble with tight curves but robot seems to be following line.

--- programs/linefollowing/linefollowing.py
+++ programs/linefollowing/linefollowing.py
@@ -1,25 +1,19 @@
 import cv2
-import Jetson.GPIO as GPIO
 import time
+import sys
+sys.path.append('../../lib')
+import motorcontrol
 
 ## ------------------ CALIBRATION --------------------
-#motors
-#motorLeftPos =
-#motorLeftNeg = 
-#motorRightPos = 
-#motorRightNeg = 
-
-#vision
-#center is 320
+#vision (center is 320)
 thresholdLeft = 220 #lower X-values are on the left side of the screen, thus we need to turn left if the line is below this value
 thresholdRight = 420 #higher X-values are on the right side of the screen, thus we need to turn right if the line is above this value
 thresholdNoLineLeft = 30 #If the line is too far right, it is past this threshold to see in order to turn left
-thresholdNoLineRight = 600 #If the line is too far left, it is past this threshold to see in order to turn right
-movingAverageLength = 1
+thresholdNoLineRight = 630 #If the line is too far left, it is past this threshold to see in order to turn right
+movingAverageLength = 2
 
 ## ----------------- GPIO SETUP ----------------------
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup([15,12,31,32], GPIO.OUT, initial=GPIO.LOW)
+motorcontrol.initializeMotorPins()
 
 ## -----------CAMERA INITIALIZATION -----------------
 cap = cv2.VideoCapture(0)
@@ -33,29 +27,25 @@ try:
     while(1):
 
         ret, frame = cap.read()
-        ## ----------------- VISION ----------------------
+        ## ----------------- VISION -----------------------
         # convert to grayscale, gaussian blur, and threshold
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray,(5,5),0)
-        cv2.imwrite("a.jpg", blur)
+        #cv2.imwrite("a.jpg", blur)
         ret,thresh1 = cv2.threshold(blur,100,255,cv2.THRESH_BINARY_INV)
-        cv2.imwrite("b.jpg", thresh1)
+        #cv2.imwrite("b.jpg", thresh1)
         # Erode to eliminate noise, Dilate to restore eroded parts of image
         mask = cv2.erode(thresh1, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
         h, w = mask.shape
         maskCloser = mask[int(2*h/3):int(h), 0:w]
         maskFarther = mask[int(1*h/3):int(2*h/3), 0:w]
-        cv2.imwrite("c.jpg", maskCloser)
+        #cv2.imwrite("c.jpg", maskCloser)
         # Find all contours in frame: Close Contour and Farther Counter
         something, contoursClose, hierarchy = cv2.findContours(maskCloser.copy(),1,cv2.CHAIN_APPROX_NONE)
         something, contoursFarther, hierarchy = cv2.findContours(maskFarther.copy(),1,cv2.CHAIN_APPROX_NONE)
 
-        # Find x-axis centroid of largest contour and cut power to appropriate motor
-        # to recenter camera on centroid.
-        # This control algorithm was written referencing guide:
-        # Author: Einsteinium Studios
-        # Availability: http://einsteiniumstudios.com/beaglebone-opencv-line-following-robot.html
+        ## --------------- MOTOR CONTROL ------------------
         if len(contoursClose) > 0:
             # Find largest contour area and image moments
             c = max(contoursClose, key = cv2.contourArea)
@@ -70,40 +60,33 @@ try:
                 print("division by zero")
             av = sum(movingaverage)/len(movingaverage)
             elapsedTime = time.time() - initialTime
-            turnsleeptime = (abs((av - (thresholdRight - thresholdLeft))/(thresholdRight - thresholdLeft)) * 0.005) + 0.005
+            turnsleeptime = (abs((av - (thresholdRight - thresholdLeft))/(thresholdRight - thresholdLeft)) * 0.003) + 0.002
 
             if av < thresholdNoLineRight and av >= thresholdRight:
                 #Condition 1 Motion: Right
-                GPIO.output(15, GPIO.LOW)
-                GPIO.output(12, GPIO.HIGH)
-                GPIO.output(31, GPIO.HIGH)
-                GPIO.output(32, GPIO.LOW)
+                motorcontrol.rightPivotTurn()
                 time.sleep(turnsleeptime)
-                GPIO.output([15,12,31,32], GPIO.LOW)
+                motorcontrol.stop()
                 print(str(round(elapsedTime)) + "    Turning Right!            Avg: " + str(round(av)) + "      Last: " + str(cx))
 
             if av < thresholdRight and av > thresholdLeft:
                 #Condition 2 Motion: Straight
-                GPIO.output(15, GPIO.HIGH)
-                GPIO.output(12, GPIO.LOW)
-                GPIO.output(31, GPIO.HIGH)
-                GPIO.output(32, GPIO.LOW)
+                motorcontrol.straight()
                 time.sleep(0.03)
-                GPIO.output([15,12,31,32], GPIO.LOW)
+                motorcontrol.stop()
                 print(str(round(elapsedTime)) + "    Going Straight!!          Avg: " + str(round(av)) + "      Last: " + str(cx))
 
             if av > thresholdNoLineLeft and av <= thresholdLeft:
                 #Condition 3 Motion: Left
-                GPIO.output(15, GPIO.HIGH)
-                GPIO.output(12, GPIO.LOW)
-                GPIO.output(31, GPIO.LOW)
-                GPIO.output(32, GPIO.HIGH)
+                motorcontrol.leftPivotTurn()
                 time.sleep(turnsleeptime + 0.005)
-                GPIO.output([15,12,31,32], GPIO.LOW)
+                motorcontrol.stop()
                 print(str(round(elapsedTime)) + "    Turning Left!             Avg: " + str(round(av)) + "      Last: " + str(cx))
 
             if av >= thresholdNoLineRight or av <= thresholdNoLineLeft:
-                GPIO.output([15,12,31,32], GPIO.LOW)
+                motorcontrol.stop()
+                print(str(round(elapsedTime)) + "    No More Line!             Avg: " + str(round(av)))
+                # If the line isnt visable, look for the line farther away
                 if len(contoursFarther) > 0:
                     # Find largest contour area and image moments
                     c = max(contoursFarther, key = cv2.contourArea)
@@ -111,35 +94,27 @@ try:
                     # Find x-axis centroid using image moments
                     try:
                         av = int(M['m10']/M['m00'])
-                        print("FURTHER VALUE FOUND")
-                        print(av)
-                        turnsleeptime = (abs((av - (thresholdRight - thresholdLeft))/(thresholdRight - thresholdLeft)) * 0.006) + 0.0075
+                        turnsleeptime = (abs((av - (thresholdRight - thresholdLeft))/(thresholdRight - thresholdLeft)) * 0.006) + 0.02
                         if av > thresholdRight:
                             #Condition 1 Motion: Right
-                            GPIO.output(15, GPIO.LOW)
-                            GPIO.output(12, GPIO.HIGH)
-                            GPIO.output(31, GPIO.HIGH)
-                            GPIO.output(32, GPIO.LOW)
+                            print(str(round(elapsedTime)) + "    F RIGHT TURN             Avg: " + str(round(av)))
+                            motorcontrol.rightPivotTurn()
                             time.sleep(turnsleeptime)
-                            GPIO.output([15,12,31,32], GPIO.LOW)
+                            motorcontrol.stop()
                         if av > 1 and av < thresholdLeft:
+                            print(str(round(elapsedTime)) + "    F LEFT TURN              Avg: " + str(round(av)))
                             #Condition 3 Motion: Left
-                            GPIO.output(15, GPIO.HIGH)
-                            GPIO.output(12, GPIO.LOW)
-                            GPIO.output(31, GPIO.LOW)
-                            GPIO.output(32, GPIO.HIGH)
+                            motorcontrol.leftPivotTurn()
                             time.sleep(turnsleeptime + 0.005)
-                            GPIO.output([15,12,31,32], GPIO.LOW)
+                            motorcontrol.stop()
                     except(ZeroDivisionError):
                         av = 0
                         print("division by zero")
-                print(str(round(elapsedTime)) + "    No More Line!             Avg: " + str(round(av)))
-
 
 except(KeyboardInterrupt,SystemExit):
     print("---- Exiting ----")
-    GPIO.output([15,12,31,32], GPIO.LOW)
     cap.release()
     cv2.destroyAllWindows()
-    GPIO.cleanup()
+    motorcontrol.stop()
+    motorcontrol.cleanup()
     print("---- Done ----")
