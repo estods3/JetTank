2763beccc442bcf84dca9d846f77685430ba1511
estods3 <estods76@gmail.com>
2020-01-18 08:20:51 -0800

    centered camera on jetbot. adjusted script to use new thresholds. not yet working.

--- programs/linefollowing/linefollowing.py
+++ programs/linefollowing/linefollowing.py
@@ -2,16 +2,32 @@ import cv2
 import Jetson.GPIO as GPIO
 import time
 
+## Calibration
+#motors
+#motorLeftPos =
+#motorLeftNeg = 
+#motorRightPos = 
+#motorRightNeg = 
+
+#vision
+thresholdLeft = 340 #higher X-values are on the right side of the screen, thus we need to turn left if the line is above this value
+thresholdRight = 300 #lower X-values are on the left side of the screen, thus we need to turn right if the line is below this value
+thresholdNoLineLeft = 30 #If the line is too far right, it is past this threshold to see in order to turn left
+thresholdNoLineRight = 600 #If the line is too far left, it is past this threshold to see in order to turn right
+movingAverageLength = 3
+
+
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup([15,12,31,32], GPIO.OUT, initial=GPIO.LOW)
-
 
 cap = cv2.VideoCapture(0)
 if not(cap.isOpened()):
     print("Could not open camera")
 
 try:
-    movingaverage = [250-95] * 5
+    movingaverage = [250-95] * movingAverageLength
+    initialTime = time.time()
     while(1):
 
         ret, frame = cap.read()
@@ -25,6 +41,8 @@ try:
         # Erode to eliminate noise, Dilate to restore eroded parts of image
         mask = cv2.erode(thresh1, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
+        h, w = mask.shape
+        mask = mask[int(0):int(h), 0:w]
         cv2.imwrite("c.jpg", mask)
         # Find all contours in frame
         something, contours, hierarchy = cv2.findContours(mask.copy(),1,cv2.CHAIN_APPROX_NONE)
@@ -39,46 +57,51 @@ try:
             c = max(contours, key = cv2.contourArea)
             M = cv2.moments(c)
             # Find x-axis centroid using image moments
-            cx = int(M['m10']/M['m00'])
-            #print(cx)
-            movingaverage.insert(0, cx)
-            movingaverage.pop()
+            try:
+                cx = int(M['m10']/M['m00'])
+                movingaverage.insert(0, cx)
+                movingaverage.pop()
+            except(ZeroDivisionError):
+                print("division by zero")
             av = sum(movingaverage)/len(movingaverage)
-            #print(cx)
-            if av >= 250:
+            elapsedTime = time.time() - initialTime
+            if av < thresholdNoLineRight and av >= thresholdRight:
                 #Condition 1 Motion: Right
                 GPIO.output(15, GPIO.LOW)
                 GPIO.output(12, GPIO.LOW)
                 GPIO.output(31, GPIO.HIGH)
                 GPIO.output(32, GPIO.LOW)
-                print("Turning Right!            Avg: " + str(round(av)) + "      Last: " + str(cx))
+                time.sleep(0.01)
+                GPIO.output([15,12,31,32], GPIO.LOW)
+                print(str(round(elapsedTime)) + "    Turning Right!            Avg: " + str(round(av)) + "      Last: " + str(cx))
 
-            if av < 250 and av > 95:
+            if av < thresholdRight and av > thresholdLeft:
                 #Condition 2 Motion: Straight
                 GPIO.output(15, GPIO.HIGH)
                 GPIO.output(12, GPIO.LOW)
                 GPIO.output(31, GPIO.HIGH)
                 GPIO.output(32, GPIO.LOW)
-                print("Going Straight!!          Avg: " + str(round(av)) + "      Last: " + str(cx))
+                time.sleep(0.05)
+                GPIO.output([15,12,31,32], GPIO.LOW)
+                print(str(round(elapsedTime)) + "    Going Straight!!          Avg: " + str(round(av)) + "      Last: " + str(cx))
 
-
-            if av <= 95:
+            if av > thresholdNoLineLeft and av <= thresholdLeft:
                 #Condition 3 Motion: Left
                 GPIO.output(15, GPIO.HIGH)
                 GPIO.output(12, GPIO.LOW)
                 GPIO.output(31, GPIO.LOW)
                 GPIO.output(32, GPIO.LOW)
-                print("Turning Left!             Avg: " + str(round(av)) + "      Last: " + str(cx))
-
-
-            if av >= 600:
+                time.sleep(0.01)
                 GPIO.output([15,12,31,32], GPIO.LOW)
-                print("No More Line!             Avg: " + str(round(av)))
+                print(str(round(elapsedTime)) + "    Turning Left!             Avg: " + str(round(av)) + "      Last: " + str(cx))
+
+            if av >= thresholdNoLineRight or av <= thresholdNoLineLeft:
+                GPIO.output([15,12,31,32], GPIO.LOW)
+                print(str(round(elapsedTime)) + "    No More Line!             Avg: " + str(round(av)))
 
 except(KeyboardInterrupt,SystemExit):
     print("---- Exiting ----")
     GPIO.output([15,12,31,32], GPIO.LOW)
-    #GPIO.output(11, GPIO.LOW)
     cap.release()
     cv2.destroyAllWindows()
     GPIO.cleanup()
